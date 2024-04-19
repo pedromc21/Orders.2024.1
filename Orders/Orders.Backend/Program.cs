46f1087702f52eea78a750d8eba2f6ef1e9a28d0
Ing. Pedro Marquez Cano <pmarquez@gissa.com.mx>
2024-04-19 11:37:08 -0600

    Add basic contries api controller

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,20 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
-using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin=>true).AllowCredentials());
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
